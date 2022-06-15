66e9091d06832a259a76619c3912f4511d44a8ce
ian.davis <ian.davis@salemwebnetwork.com>
2022-06-14 20:24:26 -0400

    .

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,68 +1,105 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import gameStore from '@/stores/gameStore'
-
-const sharedGameState = gameStore.state
 
 let modal = null
 
+const isAdmin = ref(false)
+
 onMounted(() => {
-    modal = document.getElementById("modal")
+    modal = document.getElementById("modal");
 })
 
 const displayModal = () => {
     modal.showModal()
 }
 
-const closeModal = () => {
-    modal.close()
+const closeModal = (e) => {
+    if(e.target.dataset.closeModalOnClick) {
+        modal.close()
+    }    
 }
 </script>
 
 <template>
-    <img class="settings" @click.prevent="displayModal" src="/icons8-settings.svg" width="44" height="44" />
-    <dialog class="modal" id="modal">
-        <div class="actions">
-            <button class="close-modal-btn" @click.prevent="closeModal">Close</button>
+    <div @click.prevent="displayModal" class="settings">
+        <img src="/icons8-settings.svg" width="38" height="38" />
+    </div>
+    <dialog class="modal" id="modal" @click="closeModal" data-close-modal-on-click="true">
+        <div class="text-lg">hello there</div>
+        <div class="inner">
+            <div class="adminContainer">
+                <input type="checkbox" id="admin" v-model="isAdmin" /><label for="admin">{{isAdmin}}</label>
+            </div>
         </div>
     </dialog>
 </template>
 
 <style scoped>
-    .settings { position: fixed; top: 8px; right: 8px; cursor: pointer; }
+.settings {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+}
+
+.adminContainer {
+    display: flex;
+}
 
 dialog {
-  background-color: #222831;
-  color: #F9FFEE;
-  text-align: center;
-  border: none;
-  padding: 2rem;
-  border-radius: 5px;
-  box-shadow: 0 0 40px rgba(0,0,0,0.1), 0 0 10px rgba(0,0,0,0.25);
+    background-color: #222831;
+    color: #f9ffee;
+    border: none;
+    padding: 2rem;
+    border-radius: 5px;
+    box-shadow: 0 0 40px rgba(0, 0, 0, 0.1), 0 0 10px rgba(0, 0, 0, 0.25);
+    padding:0;
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 }
 
 dialog[open] {
-  animation: appear .15s cubic-bezier(0, 1.8, 1, 1.8);
+    animation: appear 0.15s cubic-bezier(0, 1.8, 1, 1.8);
 }
 
 dialog::backdrop {
-  background: linear-gradient(45deg, rgba(0,143,104,.5), rgba(250,224,66,.5));
+    background: linear-gradient(
+        45deg,
+        rgba(0, 143, 104, 0.5),
+        rgba(250, 224, 66, 0.5)
+    );
+}
+
+dialog .inner {
+    padding: 15px; width: 300px;
 }
 
 @keyframes appear {
-  from {
-    opacity: 0;
-    transform: translateX(-3rem);
-  }
+    from {
+        opacity: 0;
+        transform: translateX(-3rem);
+    }
 
-  to {
-    opacity: 1;
-    transform: translateX(0);
-  }
+    to {
+        opacity: 1;
+        transform: translateX(0);
+    }
 }
 
-    /* TABLET AND BELOW */
-    @media screen and (max-width: 499px) {
-        .settings { width:24px; height:24px; }
+/* TABLET AND BELOW */
+@media screen and (max-width: 499px) {
+    .settings {
+        padding: 10px;
+        top: 0;
+        right: 0;
     }
+    .settings img {
+        width: 24px;
+        height: 24px;
+        float: left;
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Toaster from "@meforma/vue-toaster"
 
+import './styles/app.css'
+
 const app = createApp(App)
 
 app.use(Toaster)
